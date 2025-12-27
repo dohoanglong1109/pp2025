a722a88cd51ef298f6a92265fde006568eb2a31a
longdh <longdohoang@gmail.com>
2025-12-27 21:32:35 +0700

    Fix the name of functions

--- pw5/main.py
+++ pw5/main.py
@@ -9,10 +9,10 @@ def main():
     for s in classroom:
         s.set_gpa(s.calGPA(course_db))
 
-    input.save_students_csv(classroom)
-    input.save_courses_csv(course_db)
-    input.save_report_json(classroom)
-
+    input.save_student_to_csv(classroom)
+    input.save_course_to_csv(course_db)
+    input.save_mark_to_json(classroom)
+    
     output.print_student_list(classroom)
 
 if __name__ == "__main__":
